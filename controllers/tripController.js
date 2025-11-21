9a788a7c1a21b02423c5c7679e0535ffce22631e
Aleksandar <aleksandargvozdenovic78@gmail.com>
2025-11-21 11:30:23 +0100

    setting everything up

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,8 +1,11 @@
+import Activity from "../models/Activity.js";
 import Trip from "../models/Trip.js";
 
 export const getTrips = async (req, res, next) => {
   try {
-    const trips = await Trip.find({ user: req.user._id }).sort({ createdAt: -1 });
+    const trips = await Trip.find({ user: req.user._id }).sort({
+      createdAt: -1
+    });
     res.json(trips);
   } catch (error) {
     next(error);
@@ -11,11 +14,16 @@ export const getTrips = async (req, res, next) => {
 
 export const getTripById = async (req, res, next) => {
   try {
-    const trip = await Trip.findOne({ _id: req.params.id, user: req.user._id });
+    const trip = await Trip.findOne({
+      _id: req.params.id,
+      user: req.user._id
+    });
+
     if (!trip) {
       res.status(404);
       throw new Error("Trip not found");
     }
+
     res.json(trip);
   } catch (error) {
     next(error);
@@ -26,9 +34,9 @@ export const createTrip = async (req, res, next) => {
   try {
     const { title, destination, startDate, endDate, budget } = req.body;
 
-    if (!title || !destination) {
+    if (!title || !destination || !startDate || !endDate) {
       res.status(400);
-      throw new Error("Title and destination are required");
+      throw new Error("Please provide title, destination, start and end date");
     }
 
     const trip = await Trip.create({
@@ -48,14 +56,23 @@ export const createTrip = async (req, res, next) => {
 
 export const updateTrip = async (req, res, next) => {
   try {
-    const trip = await Trip.findOne({ _id: req.params.id, user: req.user._id });
+    const trip = await Trip.findOne({
+      _id: req.params.id,
+      user: req.user._id
+    });
+
     if (!trip) {
       res.status(404);
       throw new Error("Trip not found");
     }
 
-    const updates = req.body;
-    Object.assign(trip, updates);
+    const { title, destination, startDate, endDate, budget } = req.body;
+
+    if (title !== undefined) trip.title = title;
+    if (destination !== undefined) trip.destination = destination;
+    if (startDate !== undefined) trip.startDate = startDate;
+    if (endDate !== undefined) trip.endDate = endDate;
+    if (budget !== undefined) trip.budget = budget;
 
     const updatedTrip = await trip.save();
     res.json(updatedTrip);
@@ -66,14 +83,20 @@ export const updateTrip = async (req, res, next) => {
 
 export const deleteTrip = async (req, res, next) => {
   try {
-    const trip = await Trip.findOne({ _id: req.params.id, user: req.user._id });
+    const trip = await Trip.findOne({
+      _id: req.params.id,
+      user: req.user._id
+    });
+
     if (!trip) {
       res.status(404);
       throw new Error("Trip not found");
     }
 
+    await Activity.deleteMany({ trip: trip._id, user: req.user._id });
+
     await trip.deleteOne();
-    res.json({ message: "Trip removed" });
+    res.json({ message: "Trip and related activities removed" });
   } catch (error) {
     next(error);
   }
