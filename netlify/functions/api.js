2ef6a425d52f97212e2bbf9ccd6e0813749d60ff
Aleksandar <aleksandargvozdenovic78@gmail.com>
2025-11-22 13:49:44 +0100

    Add db config and fix Netlify serverless handler

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,25 +1,29 @@
-
 import serverless from "serverless-http";
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
-import dotenv from "dotenv";
 
 import connectDB from "../../config/db.js";
 import authRoutes from "../../routes/authRoutes.js";
 import tripRoutes from "../../routes/tripRoutes.js";
 import activityRoutes from "../../routes/activityRoutes.js";
-import { notFound, errorHandler } from "../../middleware/errorMiddleware.js";
+import { notFound, errorHandler } from "../../middleware/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
 
-
+// NEMA app.use(express.json()) ovdje
 app.use(cors());
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.get("/", (req, res) => {
+  res.json({ message: "WanderLog API (Netlify) is running" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/trips", tripRoutes);
